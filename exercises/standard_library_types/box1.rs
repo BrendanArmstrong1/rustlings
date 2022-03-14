01ea48003ca10e5981352a238aba6d1ee249c3b4
Brendan Armstrong <Brendan.Armstrong@mail.com>
2022-03-13 21:52:30 -0400

    more rustlings done

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,8 +16,6 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
     Cons(i32, List),
@@ -33,11 +31,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    List::Cons(2, List::Nil)
 }
 
 #[cfg(test)]
